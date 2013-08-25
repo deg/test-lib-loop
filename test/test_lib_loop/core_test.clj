d7f72f2a8d5dc5053fddb56cf65fd6af3a4e0214
David Goldfarb <deg@degel.com>
2013-08-25 16:34:02 +0300

    Initial files from 'lein new test-lib-loop'

---
+++ test/test_lib_loop/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-lib-loop.core-test
+  (:require [clojure.test :refer :all]
+            [test-lib-loop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
